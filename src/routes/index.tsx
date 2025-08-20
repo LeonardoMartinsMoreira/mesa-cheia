4e7d3ddddf2a51cb6a8cb6c822cc2a28a1c34d89
Leonardo Martins <leomartinsmoreira96@gmail.com>
2025-08-19 21:45:58 -0300

    created main page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,23 @@
+import { FoodCard } from '@/components/food-card'
+import { FoodVoting } from '@/components/food-voting'
+import Header from '@/components/header'
+
 export default function Home() {
-  return <h1>🏠 Home</h1>
+  return (
+    <div className="flex flex-col">
+      <Header />
+
+      <div className="flex gap-y-6 flex-col pt-15">
+        <div className="px-2">
+          <h1 className="text-3xl font-bold">Olá Samara,</h1>
+          <h1 className="text-3xl font-bold">Bom dia!</h1>
+        </div>
+
+        <div className="px-2">
+          <FoodCard />
+        </div>
+        <FoodVoting />
+      </div>
+    </div>
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
@@ -29,4 +32,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
